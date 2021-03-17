3b9cca33571ace41fdd34ce673d23b66887fdb19
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-03-17 15:28:50 +0530

    kotlin

---
+++ Kotlin/HelloKotlin/src/main/kotlin/ClassTrial.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val person = Person("Gurbaaz", "Gill")
+    println(person.firstName)
+    person.printInfo()
+    person.nickName="Nannu"
+    println(person.nickName)
+    person.printInfo()
+}
--- Kotlin/HelloKotlin/src/main/kotlin/function.kt
+++ Kotlin/HelloKotlin/src/main/kotlin/function.kt
@@ -1,6 +1,13 @@
-fun sayHello(greeting:String,itemToGreet:String)= println("$greeting $itemToGreet")
+fun sayHello(greeting:String, vararg itemsToGreet:String) {
+    itemsToGreet.forEach{ itemToGreet ->
+        println("$greeting $itemToGreet")
+    }
+}
 
 fun main() {
+//    val people = listOf("Gurbaaz", "Anhad", "Timmy")
+    sayHello("Hey","Gurbaaz", "Anhad")
+
    val interestingThings = arrayOf("Kotlin", "Comics", "Laptops")
     println(interestingThings.size)
     println(interestingThings[0])
