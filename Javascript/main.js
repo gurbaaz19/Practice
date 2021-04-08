005b20eed1648b3a36f1842704c0e716cc56961f
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-04-08 19:36:20 +0530

    variables

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+console.log("🚀Hello World");
+
+let x = 345;
+let y = "Hello";
+console.log(x, y);
+
+let z=['A',2,'B',5, 'How are you', '😏',['How are you',5]]
+console.log(z)
+console.log(z[5])
+console.log(z[6][0])
