9dc60979d8df9a52179db317f571915b6771c1df
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-04-08 22:31:09 +0530

    Functions

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -2,6 +2,16 @@ function sum(x, y, z) {
   console.log(x + y + 2 * z);
 }
 
+//Other ways to define
+
+// const sum= function (x, y, z) {
+//     console.log(x + y + 2 * z);
+//   }
+
+//   const sum= (x, y, z) => {
+//     console.log(x + y + 2 * z);
+//   }
+
 for (let p = 3; p < 10; p += 1) {
   sum(p, 0, 8);
 }
