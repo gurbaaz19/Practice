e41a4856f946c16285e4396a19d3602139863794
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-07-14 05:41:09 +0530

    first shell script

--- C++/vector.cpp
+++ C++/vector.cpp
@@ -5,10 +5,27 @@ using namespace std;
 
 int main()
 {
-    vector<int> vect{ 10, 20, 30 };
- 
-    for (int x : vect)
-        cout << x << " ";
- 
+    vector<vector<int>> vect{{5},{6,4},{2}};
+    vect.push_back({89,53,43});
+    vect.push_back({65,43,34});
+    cout<<vect.size()<<endl;
+    for (int i=0; i<vect.size();i++){
+        for (int j=0; j<vect.at(i).size();j++)
+        cout << vect.at(i).at(j) << " ";
+    }
+    cout<<"\n"<<sizeof(vect.at(0).at(0))<<endl;
+
+    int * ptr_int;
+    int * ptr_int2;
+    int * ptr_int3;
+
+    int i{1};
+    while(true){
+        ptr_int= new int[1000];
+        ptr_int2= new int[1000];
+        ptr_int3= new int[1000];
+        cout<<i<<endl;
+        i++;
+    }
     return 0;
 }
---
+++ Shell Script/first.sh
@@ -0,0 +1,9 @@
+echo Enter your name 
+read name
+echo Hi $name, How are you ?
+
+echo First argument is $1 and second is $2
+
+pi=3.14
+readonly pi
+echo Value of PI is $pi
